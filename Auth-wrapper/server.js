40756beae0f590d10d33c2d33f83550728feab41
ar1062000 <ar1062000a@gmail.com>
2026-05-20 17:43:46 +0500

    stuff

--- Auth-wrapper/server.js
+++ Auth-wrapper/server.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, "../DevBreif/dist")));
 
 /* ---------------- REACT ROUTER SUPPORT ---------------- */
 
-app.get((req, res) => {
+app.get("/",(req, res) => {
   res.sendFile(path.join(__dirname, "../DevBreif/dist/index.html"));
 });
 
